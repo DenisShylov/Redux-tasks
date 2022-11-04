4ac4bee4dae84569969c53f60ebfd8e80bcc4cbe
DenisShylov <dodjer123@gmail.com>
2022-11-04 18:52:01 +0200

    upd

--- lesson2/task1/src/index.js
+++ lesson2/task1/src/index.js
@@ -4,7 +4,7 @@ const INCREMENT = 'COUNTER/INCREMENT';
 const DECREMENT = 'COUNTER/DECREMENT';
 export const increment = () => {
   return {
-    type: 'INCREMENT',
+    type: INCREMENT,
   };
 };
 
@@ -14,7 +14,7 @@ export const decrement = () => {
   };
 };
 
-export const counterReducer = (state = 0, action) => {
+const counterReducer = (state = 0, action) => {
   switch (action.type) {
     case INCREMENT:
       return state + 1;
