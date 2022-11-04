86540e808978c72e27fa2d36bd5dc5084076163c
DenisShylov <dodjer123@gmail.com>
2022-11-04 18:50:11 +0200

    upd

--- lesson2/task1/src/index.js
+++ lesson2/task1/src/index.js
@@ -14,7 +14,7 @@ export const decrement = () => {
   };
 };
 
-const counterReducer = (state = 0, action) => {
+export const counterReducer = (state = 0, action) => {
   switch (action.type) {
     case INCREMENT:
       return state + 1;
