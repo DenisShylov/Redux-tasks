121c0820fbf5fb421362b3258a54f5455c8e417e
DenisShylov <dodjer123@gmail.com>
2022-11-05 20:57:12 +0200

    upd

--- lesson2/task2/src/store.js
+++ lesson2/task2/src/store.js
@@ -31,19 +31,16 @@ const counterReducer = (state = initialState, action) => {
   switch (action.type) {
     case INCREMENT:
       return {
-        ...state,
         value: state.value + 1,
         history: state.history.concat('+1'),
       };
     case DECREMENT:
       return {
-        ...state,
         value: state.value - 1,
         history: state.history.concat('-1'),
       };
     case RESET:
       return {
-        ...state,
         value: 0,
         history: [],
       };
