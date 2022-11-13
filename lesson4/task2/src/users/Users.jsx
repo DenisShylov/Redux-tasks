9bdbc522500f734f711376203d7cf725ec4ab52a
DenisShylov <dodjer123@gmail.com>
2022-11-13 19:24:52 +0200

    task2

---
+++ lesson4/task2/src/users/Users.jsx
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import * as userActions from './users.actions';
+import '../styles.scss';
+class Users extends Component {
+  onUserCreate = () => {
+    const id = Math.round(Math.random() * 1000000);
+
+    const newUser = {
+      id,
+      name: `User # ${id}`,
+    };
+
+    this.props.createUsers(newUser);
+  };
+  render() {
+    const { users } = this.props;
+
+    return (
+      <div className="users">
+        <button className="users__create-btn" onClick={this.onUserCreate}>
+          Create User
+        </button>
+        <ul className="users__list">
+          {users.map(({ name, id }) => (
+            <li key={id} className="users__list-item">
+              {name}
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
+}
+const mapState = (state) => {
+  return {
+    users: state.users.usersList,
+  };
+};
+
+const mapDispatch = {
+  createUsers: userActions.addUser,
+};
+
+const connector = connect(mapState, mapDispatch);
+const ConnectedUsers = connector(Users);
+export default ConnectedUsers;
